5e3c77d15fbbd5f745882b49105fe2c8c2807893
Faizan Ahmad <162777329+RizFaizan@users.noreply.github.com>
2026-01-15 17:03:53 +0500

    Environment setup

---
+++ ArrayMehods.js
@@ -0,0 +1,129 @@
+const nums = [1, 2, 3, 4, 5];
+
+const multipliedByThree = nums.map(n => n * 3);
+console.log(multipliedByThree);
+
+
+const fruits = ["apple", "banana", "mango"];
+
+const upperCased = fruits.map(n => n.toUpperCase());
+console.log(upperCased);
+
+
+const newNums = [10, 20, 30, 40, 50];
+
+const string = newNums.map(n => n.toString().concat("€"))
+console.log(string)
+
+
+const books = [
+    { title: "Harry Potter", pages: 300 },
+    { title: "The Hobbit", pages: 250 },
+    { title: "1984", pages: 328 }
+];
+
+const booksTitle = books.map(n => n.title);
+console.log(booksTitle);
+
+
+
+const temp = [0, 15, 30, 45];
+
+const tempInCelcius = temp.map(n => (n * 9 / 5) + 32);
+console.log(tempInCelcius);
+
+
+
+const arr = [1, 2, 3, 4, 5];
+
+const result = arr.map(n => n % 2 === 0 ? n * 2 : n * 3);
+console.log(result);
+
+
+
+
+// Advanced exercise
+
+const products = [
+    { name: "Laptop", price: 1000, discount: 0.1 },
+    { name: "Phone", price: 500, discount: 0.2 },
+    { name: "Tablet", price: 300, discount: 0.05 }
+];
+
+
+const updatedProducts = products.map(n => ({
+    name: n.name,
+    finalPrice: n.price * (1 - n.discount)
+}));
+
+console.log(updatedProducts)
+
+
+
+const numbers = [1, 2, 3, 4];
+const multipliers = [10, 20, 30, 40];
+
+const finalResult = numbers.map((nItems, mMultiplier) => nItems * multipliers[mMultiplier]);
+console.log(finalResult);
+
+
+
+
+const arrOfString = ["JavaScript", "is", "awesome"];
+const newResult = arrOfString.map(str => {
+    return {
+        word: str,
+        length: str.length,
+        islong: str.length > 5
+    }
+})
+
+console.log(newResult);
+
+
+
+const givenArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const even = givenArray.filter(n => n % 2 === 0);
+
+console.log(even);
+
+
+const fruitsArray = ["apple", "banana", "cherry", "date", "elderberry"];
+const resultFruit = fruitsArray.filter(n => n.length >= 5);
+
+console.log(resultFruit);
+
+
+
+
+const number = [15, -3, 0, 8, -10, 12];
+const finalNum = number.filter(n => n >= 0);
+
+console.log(finalNum);
+
+
+
+
+const students = [
+    { name: "Alice", grade: 85 },
+    { name: "Bob", grade: 45 },
+    { name: "Charlie", grade: 92 },
+    { name: "Diana", grade: 78 }
+];
+
+const passed = students.filter(n => n.grade >= 60)
+console.log(passed);
+
+
+
+const arrOfWords = ["hello", "world", "javascript", "filter", "method", "array", "ethanol"];
+const arrOfVowels = arrOfWords.filter(n => n.length >= 4 & n.startsWith("a"));
+
+console.log(arrOfVowels);
+
+
+
+const arrOfNums = [12, 34, 56, 78, 90];
+const sumOfNums = arrOfNums.filter(n => n.toString().split('').map(Number).reduce((sum, acc) => sum + acc, 0) > 10);
+
+console.log(sumOfNums);
---
+++ script.js
@@ -0,0 +1,190 @@
+// const numbers = [1, 2, 3, 4, 5];
+
+// const [first, ...rest] = numbers;
+
+// console.log(first); // 1
+// console.log(rest);  // [2,3,4,5]
+
+
+
+
+// const nums = [1, 2, 3];
+// const squared = nums.map(n => n * n);
+
+// console.log(squared);
+
+
+
+// const users = [{ user: "faizan", id: 1 }, { user: "riz", id: 3 }, { user: "sehar", id: 2 }]
+
+// const index = users.findIndex(i => i.id === 3);
+
+// console.log(index);
+
+
+// const user = users.find(u => u.id === 3);
+
+// console.log(user)
+
+
+
+// const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const even = nums.filter(u => u % 2 === 0);
+// const odd = nums.filter(u => u % 2 !== 0)
+
+// console.log(even);
+// console.log(odd);
+
+
+// const users = ['Faizan', 'Saeednan', 'Gulshan', 'Ifra'];
+
+// const fUsers = users.filter(u => u.startsWith('F'));
+
+// console.log(fUsers);
+
+
+
+// const cart = [
+//     { price: 500, qty: 2 },
+//     { price: 1000, qty: 1 },
+//     { price: 5000, qty: 3 }
+// ]
+
+// const total = cart.reduce(
+//     (sum, items) => sum + items.price * items.qty, 0
+// );
+
+// console.log(total);
+
+
+
+// const user = { userName: "Faizan", age: 19 };
+
+// const { userName: naam, age: umar } = user;
+
+// console.log(naam);
+
+
+
+// const colors = ["green", "red", "purple"]
+
+// const [first, second, third] = colors
+
+// console.log(first);
+
+
+
+
+// const numbers = [1, 2, 3, 4, 5, 6];
+
+// const [first, ...rest] = numbers;
+
+// console.log(first);
+// console.log(rest);
+
+
+// const userDetails = { userName: "Faizan", age: 18, role: "Full-Stack Dev" };
+
+// const { userName, ...otherDetails } = userDetails;
+
+// console.log(otherDetails);
+
+
+
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+// const numberThatIDontWant = numbers.filter(n => n % 2 !== 0);
+
+// console.log(numberThatIDontWant);
+
+
+
+// const user = {
+//     userName: "Faizan",
+//     age: 19,
+//     otherData: {
+//         cast: "Malik",
+//         gender: "Male"
+//     }
+// };
+
+// const { otherData: { cast, gender } } = user;
+
+// console.log(cast);
+
+
+
+// function printUser({ userName, age }) {
+//     console.log(userName);
+//     console.log(age);
+// };
+
+
+// printUser({ userName: "Faizan", age: 19 });
+
+
+
+
+// const nums = [1, 2, 3];
+// const squared = nums.map(n => n * n);
+
+// console.log(squared);
+
+
+
+
+
+// async function getUser() {
+//     const res = await fetch('https://jsonplaceholder.typicode.com/todos/1').then(resolve => )
+//     const data = await res.json();
+//     console.log(data);
+// }
+
+// getUser();
+
+
+
+// fetch('https://jsonplaceholder.typicode.com/todos')
+//     .then(response => response.json())
+//     .then(data => console.log('Data:', data))
+//     .catch(error => console.log('Error:', error));
+
+
+
+// This is the example for understanding `this` keyword and it's behaviour
+
+// const user = {
+//     userName: "Faizan",
+//     mail: "faizan@gmail.com",
+//     age: 19,
+//     meet: function () {
+//         console.log(this.age)
+//     },
+//     otherData: {
+//         height: "5'10''",
+//         skinTone: "Brownish",
+//         hairColor: "Dark Black",
+//         greet: function () {
+//             console.log(this.height);
+//         }
+//     }
+// }
+
+// user.meet();
+// user.otherData.greet();
+
+
+// Inherited value from parent scope or surrounding scope (talking about `this` kehword behaviour as lexically bound for arrow function.)
+
+// const person = {
+//     name: "John",
+//     greet: function () {
+//         setTimeout(() => {
+//             console.log(this.name);
+//         }, 1000);
+//     }
+// };
+
+// person.greet();
